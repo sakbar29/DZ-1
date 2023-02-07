21deea563d21394e069339370b8def31680f88fc
akbar sydykov <sydykovakbar966@gmail.com>
2023-02-07 22:40:53 +0600

    DZ-1/Lesson-2

---
+++ main.js
@@ -0,0 +1,40 @@
+//DZ-1
+// var arr = [10, 20, 30, 50, 235, 3000]
+//
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[3])
+// console.log(arr[4])
+
+//DZ-2
+
+// for (var i = 20; i >=0; i--){
+//     console.log(i)
+// }
+
+
+
+//DZ-3
+
+
+// var color = prompt("напишите цвета светофраа по порядку");
+// var red = 10
+// var yellow = 9
+// var green = 8
+//
+// if(red >=10) {
+//     alert("STOP!!!");
+// }else{
+//     alert("Введите цвет!!!");
+// }if(yellow < 10) {
+//     prompt("напишите цвета светофраа по порядку")
+//     alert("GET READY")
+// }else{
+//     alert("Введите цвет!!!");
+// }if (green < 10){
+//     prompt("напишите цвета светофраа по порядку")
+//     alert("START!!!")
+// }else{
+//     alert("Введите цвет!!!");
+// }
+
